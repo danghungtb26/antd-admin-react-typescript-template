6f0989e4fcb77bead6e2ba2f46cb1e8b926bc90a
hungdv <hungdv@kaopiz.com>
2024-04-10 15:51:07 +0700

    add setting cookie

--- src/commons/cookies.ts
+++ src/commons/cookies.ts
@@ -1,4 +1,4 @@
-import { AUTHEN_TOKEN_KEY } from '@constants/key'
+import { AUTHEN_TOKEN_KEY, SIDE_BAR_COLLAPSED_KEY } from '@constants/key'
 import Cookies from 'js-cookie'
 
 export const getAuthenToken = () => {
@@ -7,4 +7,12 @@ export const getAuthenToken = () => {
 
 export const setAuthenToken = (value: any) => {
   return Cookies.set(AUTHEN_TOKEN_KEY, value)
+}
+
+export const getSidebarCollapsed = () => {
+  return !!Number(Cookies.get(SIDE_BAR_COLLAPSED_KEY))
+}
+
+export const setSidebarCollapsed = (value: boolean) => {
+  return Boolean(Cookies.set(SIDE_BAR_COLLAPSED_KEY, `${value ? 1 : 0}`))
 }
--- src/contexts/setting/provider.tsx
+++ src/contexts/setting/provider.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { SettingContext, SettingContextType } from './context'
+import { getSidebarCollapsed, setSidebarCollapsed } from '@commons/cookies'
 
 type SettingProviderProps = {}
 
 const SettingProvider: React.FC<React.PropsWithChildren<SettingProviderProps>> = ({ children }) => {
-  const [sidebarCollapsed, setCollapsed] = useState<SettingContextType['sidebarCollapsed']>(false)
+  const [sidebarCollapsed, setCollapsed] =
+    useState<SettingContextType['sidebarCollapsed']>(getSidebarCollapsed())
 
   const toggleSidebarCollapsed = () => {
     setCollapsed(s => !s)
@@ -15,6 +17,10 @@ const SettingProvider: React.FC<React.PropsWithChildren<SettingProviderProps>> =
   const toggleDrawerOpened = () => {
     setDrawerOpend(s => !s)
   }
+
+  useEffect(() => {
+    setSidebarCollapsed(sidebarCollapsed)
+  }, [sidebarCollapsed])
 
   const value = useMemo<SettingContextType>(
     () => ({
